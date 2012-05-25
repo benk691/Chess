11a5cc0ffbf87cb76b4213fef7d17b0a6760fe97
Benjamin Kellogg <benk691@gmail.com>
2012-05-25 09:40:21 -0700

    Modified SMs and added pin map code

---
+++ bin/PinMap.h
@@ -0,0 +1,25 @@
+#ifndef __PINMAP_H__
+#define __PINMAP_H__
+
+/********************* Keypad Pin Map *********************/
+
+#define KEYPAD_PORT PORTC
+#define KEYPAD_PIN  PINC
+
+/**********************************************************/
+
+/********************* Replay Button Pin Map *********************/
+
+#define REPLAY_BUTTON_PORT PINA
+#define REPLAY_BUTTON_PIN  0
+
+/*****************************************************************/
+
+/********************* No Replay Button Pin Map *********************/
+
+#define NREPLAY_BUTTON_PORT PINA
+#define NREPLAY_BUTTON_PIN  1
+
+/********************************************************************/
+
+#endif //__PINMAP_H__
